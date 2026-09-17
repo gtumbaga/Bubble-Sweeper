b35a5355620e4e0ecfc4a9bb29a232fdaf341237
Gabriel Tumbaga <gabrieltumbaga@Gabriels-MacBook-Air.local>
2026-09-17 15:54:44 -0700

    trying to fix double tap zoom in on safari

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // Bumping CACHE_NAME invalidates all previously cached assets and forces
 // a fresh download on the next load - do this whenever any cached file changes.
-const CACHE_NAME = 'bubble-sweeper-v1';
+const CACHE_NAME = 'bubble-sweeper-v2';
 
 const ASSETS_TO_CACHE = [
   './',
